638bbfa5617033cc298a5dbd565b2a645cdb9237
Nicholas Alayra <nalayra@hotmail.com>
2021-11-23 09:05:07 -0800

    commit tree 4304ba6ae8eebb94a0af7b3474ea9c08890df084

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    return Math.abs(number - 42);
+}
+
+function distanceFromHqInFeet(number) {
+    return distanceFromHqInBlocks(number) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+function calculatesFarePrice(start, end) {
+    let fare;
+    if (distanceTravelledInFeet(start, end) <= 400) {
+        return fare = 0;
+    }
+    else if (distanceTravelledInFeet(start, end) <= 2000) {
+        return fare = (distanceTravelledInFeet(start, end) - 400) * .02;
+    }
+    else if (distanceTravelledInFeet(start, end) <= 2500 && distanceTravelledInFeet(start, end) > 2000) {
+        return fare = 25;
+    }
+    else {
+        return fare = 'cannot travel that far';
+    }
+}
